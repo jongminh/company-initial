cea82bac90af54c0597abc863c9acebe4681b592
Jason Hong <jaisonhong@gmail.com>
2018-01-17 14:04:50 -0500

    phase 1 WIP

---
+++ src/main/java/io/rscale/training/company/config/DataSourceConfig.java
@@ -0,0 +1,40 @@
+package io.rscale.training.company.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("cloud")
+public class DataSourceConfig extends AbstractCloudConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
+
+    public DataSourceConfig() {
+        logger.info(this.getClass() + " loaded");
+    }
+
+    @Bean
+    public DataSource dataSource() throws Exception {
+        DataSource dataSource = cloud().getSingletonServiceConnector(DataSource.class, null);
+        if ( !isMySQL(dataSource)) {
+            logger.error("MySQL required when running cloud profile.");
+            throw new UnsatisfiedDependencyException("javax.sql.DataSource", "javax.sql.DataSource", "javax.sql.DataSource", "MySQL required when running cloud profile.");
+        }
+        return dataSource;
+    }
+
+    private boolean isMySQL(DataSource dataSource) throws SQLException {
+    	return dataSource.getConnection().getMetaData().getDriverName().toLowerCase().contains("mysql");
+    	//return dataSource.getConnection().getMetaData().getDatabaseProductName().contains("MySQL");
+    }
+
+}
